fd922b8e5c553b11928d90f5a7dc3c76314f4709
rammy427 <grex4274@gmail.com>
2025-03-23 15:56:54 -0400

    Atrapando personaje en el tablero.

--- fighter.py
+++ fighter.py
@@ -8,8 +8,21 @@ class Fighter:
         self.grid_pos = grid_pos
         self.hp = 100
 
-    def move(self, delta: pygame.Vector2):
+    def move(self, delta: pygame.Vector2) -> None:
         self.grid_pos += delta
+        self.clampToBoard()
 
     def draw(self, screen: pygame.Surface) -> None:
         self.brd.getTileAt(self.grid_pos).drawFighter(screen)
+
+    # Atrapar personaje en el tablero.
+    def clampToBoard(self) -> None:
+        if self.grid_pos.x < 0:
+            self.grid_pos.x = 0
+        elif self.grid_pos.x >= self.brd.width:
+            self.grid_pos.x = self.brd.width - 1
+
+        if self.grid_pos.y < 0:
+            self.grid_pos.y = 0
+        elif self.grid_pos.y >= self.brd.height:
+            self.grid_pos.y = self.brd.height - 1
